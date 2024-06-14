fb9132a825270525124b079aa56ab3ea8aa0bac5
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2024-06-14 23:50:54 +0330

    Add files via upload

---
+++ JavaScript_ Webdesign_ Secssion28/05_Script.js
@@ -0,0 +1,47 @@
+// JavaScript Variables can be declared in 4 ways:
+/* Automatically
+   Using var
+   Using let
+   Using const */
+// x = 6;
+// y = 6;
+// z = x + y;
+// // console.log(z);
+// document.getElementById("demo1").innerHTML = "The value of z is: " + z;
+
+// // --------------------------------------------------------------------------------------
+
+// var x = 5;
+// var y = 6;
+// var z = x + y;
+// // console.log(z);
+// document.getElementById("demo1").innerHTML = "The value of z is: " + z;
+
+// // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// let x = 3;
+// let y = 2;
+// let z = x + y;
+// // console.log(z);
+// document.getElementById("demo1").innerHTML = "The value of z is: " + z;
+
+// // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const x = 3;
+const y = 3;
+const z = x + y;
+// console.log(z);
+document.getElementById("demo1").innerHTML = "The value of z is: " + z;
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const price1 = 8;
+const price2 = 6;
+let total = price1 + price2;
+document.getElementById("demo1").innerHTML ="The total is: " + total;
+
+/*The two variables price1 and price2 are declared with the const keyword.
+These are constant values and cannot be changed.
+The variable total is declared with the let keyword.
+The value total can be changed.*/
+
